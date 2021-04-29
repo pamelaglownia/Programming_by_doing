e6b4fe44ab649c855980adbe0298c7cda46b32a5
Pam <pamela.glownia@gmail.com>
2021-04-29 19:32:46 +0200

    Add tries counter to the method

--- src/pl/glownia/pamela/GuessingGame.java
+++ src/pl/glownia/pamela/GuessingGame.java
@@ -6,23 +6,30 @@ import java.util.Scanner;
 public class GuessingGame {
     public static void main(String[] args) {
         Random random = new Random();
-        int myNumber = 1 + random.nextInt(10);
+        int myNumber = 1 + random.nextInt(100);
         Scanner scan = new Scanner(System.in);
-        System.out.println("Guess number from 1 to 10:");
+        System.out.println("I'm thinking of a number between 1-100. You have 7 guesses.");
+        System.out.print("First guess: ");
         int userGuess = scan.nextInt();
-        if (userGuess < 1 || userGuess > 10) {
-            System.out.println("You can choose number from 0 to 10.");
+        int count = 1;
+        if (userGuess < 1 || userGuess > 100) {
+            System.out.println("You can choose number from 1 to 100. PLay again.");
         }
-        while (userGuess != myNumber) {
+        while (userGuess != myNumber && count < 7) {
             if (userGuess < myNumber) {
                 System.out.println("My number is greater than your guess.");
             }
             if (userGuess > myNumber) {
                 System.out.println("My number is lower than your guess.");
             }
-            System.out.println("Try again: ");
+            count++;
+            System.out.print("\nGuess #" + count + ": ");
             userGuess = scan.nextInt();
         }
-        System.out.println("Winner!");
+        if (userGuess == myNumber) {
+            System.out.println("Winner! You needed " + count + " tries.");
+        } else {
+            System.out.println("You lose :(");
+        }
     }
 }
