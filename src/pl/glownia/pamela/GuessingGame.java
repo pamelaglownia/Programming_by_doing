798540692cea6badb6107ede8f126ec8b221e525
Pam <pamela.glownia@gmail.com>
2021-04-30 22:58:53 +0200

    Change while loop to do-while loop

--- src/pl/glownia/pamela/GuessingGame.java
+++ src/pl/glownia/pamela/GuessingGame.java
@@ -5,31 +5,38 @@ import java.util.Scanner;
 
 public class GuessingGame {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Random random = new Random();
         int myNumber = 1 + random.nextInt(100);
-        Scanner scan = new Scanner(System.in);
+        int userGuess;
+        int count = 0;
         System.out.println("I'm thinking of a number between 1-100. You have 7 guesses.");
-        System.out.print("First guess: ");
-        int userGuess = scan.nextInt();
-        int count = 1;
-        if (userGuess < 1 || userGuess > 100) {
-            System.out.println("You can choose number from 1 to 100. PLay again.");
-        }
-        while (userGuess != myNumber && count < 7) {
-            if (userGuess < myNumber) {
-                System.out.println("My number is greater than your guess.");
-            }
-            if (userGuess > myNumber) {
-                System.out.println("My number is lower than your guess.");
-            }
-            count++;
-            System.out.print("\nGuess #" + count + ": ");
+        do {
+            System.out.print("Your guess: ");
             userGuess = scan.nextInt();
-        }
+            count++;
+            if (userGuess != myNumber) {
+                if (userGuess < 1 || userGuess > 100) {
+                    System.out.println("You can choose number from 1 to 100.");
+                }
+                else if (userGuess < myNumber) {
+                    System.out.println("My number is greater than your guess.");
+                }
+                else if (userGuess > myNumber) {
+                    System.out.println("My number is lower than your guess.");
+                }
+            }
+        } while (userGuess != myNumber && count < 7);
         if (userGuess == myNumber) {
-            System.out.println("Winner! You needed " + count + " tries.");
-        } else {
-            System.out.println("You lose :(");
+            if (count == 1) {
+                System.out.println("Winner! You needed " + count + " try.");
+            }
+            else {
+                System.out.println("Winner! You needed " + count + " tries.");
+            }
+        }
+        else {
+            System.out.println("\nYou lose :(");
         }
     }
 }
