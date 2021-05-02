0a9c542cea28f87289ba84543448f5a14b6bb888
Pam <pamela.glownia@gmail.com>
2021-05-02 23:27:50 +0200

    Add two players option

--- src/pl/glownia/pamela/Nim.java
+++ src/pl/glownia/pamela/Nim.java
@@ -6,30 +6,73 @@ public class Nim {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int firstPile = 3;
-        int secondPile = 3;
-        int thirdPile = 3;
+        int secondPile = 4;
+        int thirdPile = 5;
+        int number;
+        int sum;
+        String userInput;
+        String winner = "";
         System.out.println("Welcome to Nim game.There are 3 piles with matches.");
-        while (!(firstPile <= 0 && secondPile <= 0 && thirdPile <= 0)) {
-            System.out.println("A: " + firstPile + "\tB: " + secondPile + "\tC: " + thirdPile);
-            System.out.print("Choose the pile: ");
-            String userInput = scan.next();
-            while (!(userInput.equalsIgnoreCase("A")) && !(userInput.equalsIgnoreCase("B")) && !(userInput.equalsIgnoreCase("C"))) {
-                System.out.print("Incorrect value. Choose one of the piles [A, B or C]: ");
+        System.out.print("Player 1, enter your name: ");
+        String playerOne = scan.next();
+        System.out.print("Player 2, enter your name: ");
+        String playerTwo = scan.next();
+        while (!(firstPile == 0 && secondPile == 0 && thirdPile == 0)) {
+            sum = firstPile + secondPile + thirdPile;
+            if (sum == 1) {
+                winner = playerTwo;
+            }
+            if (sum > 0) {
+                System.out.println("A: " + firstPile + "\tB: " + secondPile + "\tC: " + thirdPile);
+                System.out.print(playerOne + ", choose the pile: ");
                 userInput = scan.next();
+                while (!(userInput.equalsIgnoreCase("A")) && !(userInput.equalsIgnoreCase("B")) && !(userInput.equalsIgnoreCase("C"))) {
+                    System.out.print("Incorrect value. Choose one of the piles [A, B or C]: ");
+                    userInput = scan.next();
+                }
+                System.out.print("How many matches remove from pile " + userInput.toUpperCase() + ": ");
+                number = scan.nextInt();
+                if (userInput.equalsIgnoreCase("A")) {
+                    firstPile -= number;
+                    sum -= number;
+                }
+                else if (userInput.equalsIgnoreCase("B")) {
+                    secondPile -= number;
+                    sum -= number;
+                }
+                else if (userInput.equalsIgnoreCase("C")) {
+                    thirdPile -= number;
+                    sum -= number;
+                }
             }
-            System.out.print("How many matches remove from pile " + userInput.toUpperCase() + ": ");
-            int number = scan.nextInt();
-            if (userInput.equalsIgnoreCase("A")) {
-                firstPile -= number;
+            if (sum == 1) {
+                winner = playerOne;
             }
-            else if (userInput.equalsIgnoreCase("B")) {
-                secondPile -= number;
-            }
-            else if (userInput.equalsIgnoreCase("C")) {
-                thirdPile -= number;
+            if (sum > 0) {
+                System.out.println("A: " + firstPile + "\tB: " + secondPile + "\tC: " + thirdPile);
+                System.out.print(playerTwo + ", choose the pile: ");
+                userInput = scan.next();
+                while (!(userInput.equalsIgnoreCase("A")) && !(userInput.equalsIgnoreCase("B")) && !(userInput.equalsIgnoreCase("C"))) {
+                    System.out.print("Incorrect value. Choose one of the piles [A, B or C]: ");
+                    userInput = scan.next();
+                }
+                System.out.print("How many matches remove from pile " + userInput.toUpperCase() + ": ");
+                number = scan.nextInt();
+                if (userInput.equalsIgnoreCase("A")) {
+                    firstPile -= number;
+                    sum -= number;
+                }
+                else if (userInput.equalsIgnoreCase("B")) {
+                    secondPile -= number;
+                    sum -= number;
+                }
+                else if (userInput.equalsIgnoreCase("C")) {
+                    thirdPile -= number;
+                    sum -= number;
+                }
             }
         }
-        System.out.println("A: " + firstPile + "\tB: " + secondPile + "\tC: " + thirdPile);
-        System.out.println("All piles are empty. Good job!");
+        System.out.println("\nA: " + firstPile + "\tB: " + secondPile + "\tC: " + thirdPile);
+        System.out.println(winner + " you are the winner!");
     }
 }
