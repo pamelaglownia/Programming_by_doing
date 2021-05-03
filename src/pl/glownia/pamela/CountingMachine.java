e20c3d73526ba138bf24461ee0316ac5a8a0d971
Pam <pamela.glownia@gmail.com>
2021-05-03 18:09:28 +0200

    Add posibility of providing numbers by user.

--- src/pl/glownia/pamela/CountingMachine.java
+++ src/pl/glownia/pamela/CountingMachine.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class CountingMachine {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Count from: ");
+        int fromCount = scan.nextInt();
         System.out.print("Count to: ");
-        int number = scan.nextInt();
-        for (int i = 0; i <= number; i++) {
+        int toCount = scan.nextInt();
+        System.out.print("Count by: ");
+        int byCount = scan.nextInt();
+        for (int i = fromCount; i <= toCount; i += byCount) {
             System.out.print(i + " ");
         }
     }
