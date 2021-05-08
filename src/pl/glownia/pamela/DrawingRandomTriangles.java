047f4d39feaca6c5cc022f12ea89d53783c86138
Pam <pamela.glownia@gmail.com>
2021-05-08 18:39:16 +0200

    Set right triangle and each the same size

--- src/pl/glownia/pamela/DrawingRandomTriangles.java
+++ src/pl/glownia/pamela/DrawingRandomTriangles.java
@@ -8,22 +8,17 @@ public class DrawingRandomTriangles extends Canvas {
 
     public void paint(Graphics g){
         Random random = new Random();
-
         Polygon randomTriangle = new Polygon();
 
         for(int count = 0; count <=500; count++){
             int red = random.nextInt(255);
             int blue = random.nextInt(255);
             int green = random.nextInt(255);
-            int x1 = random.nextInt(600);
-            int x2 = random.nextInt(600);
-            int x3 = random.nextInt(600);
-            int y1= random.nextInt(400);
-            int y2= random.nextInt(400);
-            int y3= random.nextInt(400);
-            randomTriangle.addPoint(x1,y1);
-            randomTriangle.addPoint(x2,y2);
-            randomTriangle.addPoint(x3,y3);
+            int x = random.nextInt(550);
+            int y= random.nextInt(300);
+            randomTriangle.addPoint(x,y);
+            randomTriangle.addPoint(x, y+50);
+            randomTriangle.addPoint(x+50, y+50);
             Color randomColor = new Color(red,green,blue);
             g.setColor(randomColor);
             g.fillPolygon(randomTriangle);
