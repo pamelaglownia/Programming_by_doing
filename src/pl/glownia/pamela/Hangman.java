19c7f05cc823112a8f98bf2bd99d97c36fc0dc11
Pam <pamela.glownia@gmail.com>
2021-05-25 19:35:50 +0200

    Checking if user wins or looses

--- src/pl/glownia/pamela/Hangman.java
+++ src/pl/glownia/pamela/Hangman.java
@@ -7,22 +7,28 @@ public class Hangman {
     public static void main(String[] args) {
         String[] fruitWords = {"cranberry", "watermelon", "orange", "pineapple", "strawberry", "grapefruit"};
         String randomWord = getRandomWord(fruitWords);
-        char[] charArray = randomWord.toCharArray();
+        char[] charArrayToHide = randomWord.toCharArray();
         int tries = 0;
+        int remainingLetters = randomWord.length();
         char guess;
-        char[] hiddenWord = hideWord(charArray);
+        char[] hiddenWord = hideWord(charArrayToHide);
         char[] supportArray = new char[randomWord.length()];
         char[] misses = hideMissedLetters(supportArray);
-
-        while (tries < randomWord.length()) {
-            System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        while (remainingLetters > 0 && tries < randomWord.length()) {
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             System.out.print("Word: ");
             System.out.println(new String(hiddenWord));
             System.out.print("Misses: ");
             System.out.println(new String(misses));
             guess = userGuess();
-            checkLetter(randomWord, hiddenWord, guess);
+            remainingLetters = checkLetter(randomWord, hiddenWord, guess, remainingLetters);
             tries = addMissingLetter(randomWord, guess, misses, tries);
+        }
+        if (remainingLetters == 0) {
+            System.out.println(randomWord);
+            System.out.println("You WIN!");
+        } else {
+            System.out.println("You loose. The correct word was " + randomWord + ".");
         }
     }
 
@@ -43,9 +49,7 @@ public class Hangman {
     public static String getRandomWord(String[] array) {
         Random random = new Random();
         int randomIndex = random.nextInt(6);
-        String randomWord = array[randomIndex];
-        System.out.println(randomWord);
-        return randomWord;
+        return array[randomIndex];
     }
 
     public static char userGuess() {
@@ -54,12 +58,14 @@ public class Hangman {
         return scan.next().charAt(0);
     }
 
-    public static void checkLetter(String randomWord, char[] hiddenWord, char guess) {
+    public static int checkLetter(String randomWord, char[] hiddenWord, char guess, int remainingLetters) {
         for (int i = 0; i < randomWord.length(); i++) {
             if (randomWord.charAt(i) == guess) {
                 hiddenWord[i] = guess;
+                remainingLetters -= 1;
             }
         }
+        return remainingLetters;
     }
 
     public static int addMissingLetter(String randomWord, char guess, char[] misses, int tries) {
