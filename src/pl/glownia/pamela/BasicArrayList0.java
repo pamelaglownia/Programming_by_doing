2d09f2f3180069cde92c8ca536ac415e076b2c27
Pam <pamela.glownia@gmail.com>
2021-05-30 15:02:27 +0200

    Solution of basic Arraylist0

---
+++ src/pl/glownia/pamela/BasicArrayList0.java
@@ -0,0 +1,29 @@
+package pl.glownia.pamela;
+
+import java.util.ArrayList;
+
+public class BasicArrayList0 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(-113);
+        System.out.println("Slot 0 contains a " + numbers.get(0));
+        numbers.add(-113);
+        System.out.println("Slot 1 contains a " + numbers.get(1));
+        numbers.add(-113);
+        System.out.println("Slot 2 contains a " + numbers.get(2));
+        numbers.add(-113);
+        System.out.println("Slot 3 contains a " + numbers.get(3));
+        numbers.add(-113);
+        System.out.println("Slot 4 contains a " + numbers.get(4));
+        numbers.add(-113);
+        System.out.println("Slot 5 contains a " + numbers.get(5));
+        numbers.add(-113);
+        System.out.println("Slot 6 contains a " + numbers.get(6));
+        numbers.add(-113);
+        System.out.println("Slot 7 contains a " + numbers.get(7));
+        numbers.add(-113);
+        System.out.println("Slot 8 contains a " + numbers.get(8));
+        numbers.add(-113);
+        System.out.println("Slot 9 contains a " + numbers.get(9));
+    }
+}
