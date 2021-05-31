b1b3abe8d631ff3ac7ec25d4747653b0764018db
Pam <pamela.glownia@gmail.com>
2021-05-31 18:22:51 +0200

    Solution of arrayList where is it

---
+++ src/pl/glownia/pamela/ArrayListWhereIsIt.java
@@ -0,0 +1,30 @@
+package pl.glownia.pamela;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayListWhereIsIt {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int element = 1 + random.nextInt(50);
+            arrayList.add(element);
+        }
+        System.out.println("ArrayList: " + arrayList);
+        System.out.print("Value to find: ");
+        int userInput = scan.nextInt();
+        if (!(arrayList.contains(userInput))) {
+            System.out.println(userInput + " is not in the ArrayList.");
+        }
+        else {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i) == userInput) {
+                    System.out.println(userInput + " is in slot " + i + ".");
+                }
+            }
+        }
+    }
+}
