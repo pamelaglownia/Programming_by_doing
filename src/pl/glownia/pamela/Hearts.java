dc2c873a46fdeff90681e15dbb8acdcf754bda53
Pam <pamela.glownia@gmail.com>
2021-06-04 13:46:07 +0200

    Shuffle deck of cards

--- src/pl/glownia/pamela/Hearts.java
+++ src/pl/glownia/pamela/Hearts.java
@@ -1,5 +1,7 @@
 package pl.glownia.pamela;
 
+import java.util.*;
+
 class Card {
     String suit;
     String rank;
@@ -11,30 +13,33 @@ class Card {
 
     @Override
     public String toString() {
-        return "suit: " + suit + ", rank: " + rank;
+        return "suit: " + suit + ", rank: " + rank + "\n";
     }
 }
 
 public class Hearts {
-    static Card[] deck;
+    static ArrayList<Card> deck;
 
     public static void main(String[] args) {
-        deck = createDeck();
+        deck = shuffleCards();
     }
 
-
-    public static Card[] createDeck() {
-        int idCard = 0;
-        deck = new Card[52];
+    public static ArrayList<Card> createDeck() {
+        deck = new ArrayList<>();
         String[] suit = {"clubs", "diamonds", "hearts", "spades"};
         String[] rank = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
         for (int i = 0; i < suit.length; i++) {
             for (int j = 0; j < rank.length; j++) {
-                deck[i] = new Card(suit[i], rank[j]);
-                System.out.println(deck[i]);
-                idCard++;
+                deck.add(new Card(suit[i], rank[j]));
             }
         }
+        return deck;
+    }
+
+    public static ArrayList<Card> shuffleCards() {
+        deck = createDeck();
+        Collections.shuffle(deck);
+        System.out.println(deck);
         return deck;
     }
 }
