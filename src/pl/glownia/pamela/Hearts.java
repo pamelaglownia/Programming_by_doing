20f4581fb975e816d5913bf9bff0ff80dece623d
Pam <pamela.glownia@gmail.com>
2021-06-05 18:52:14 +0200

    Prepare cards for players to play

--- src/pl/glownia/pamela/Hearts.java
+++ src/pl/glownia/pamela/Hearts.java
@@ -23,17 +23,42 @@ public class Hearts {
     public static void main(String[] args) {
 
         deck = shuffleCards();
+        System.out.println("Player 1, your cards:");
         ArrayList<Card> userDeckPlayerOne = dealCardsForPlayers();
-       ArrayList<Card> additionalDeck = passThreeCardsToNextPlayer(userDeckPlayerOne);
+        ArrayList<Card> additionalDeckOne = passThreeCardsToNextPlayer(userDeckPlayerOne);
+        deletePassedCards(userDeckPlayerOne, additionalDeckOne);
+        System.out.println("Player 2, your cards:");
+        ArrayList<Card> userDeckPlayerTwo = dealCardsForPlayers();
+        ArrayList<Card> additionalDeckTwo = passThreeCardsToNextPlayer(userDeckPlayerTwo);
+        deletePassedCards(userDeckPlayerTwo, additionalDeckTwo);
+
+        System.out.println("Player 3, your cards:");
+        ArrayList<Card> userDeckPlayerThree = dealCardsForPlayers();
+        ArrayList<Card> additionalDeckThree = passThreeCardsToNextPlayer(userDeckPlayerThree);
+        deletePassedCards(userDeckPlayerThree, additionalDeckThree);
+        System.out.println("Player 4, your cards:");
+        ArrayList<Card> userDeckPlayerFour = dealCardsForPlayers();
+        ArrayList<Card> additionalDeckFour = passThreeCardsToNextPlayer(userDeckPlayerFour);
+        deletePassedCards(userDeckPlayerFour, additionalDeckFour);
+
+        System.out.println("Player 1:");
+        userDeckPlayerOne = prepareUserDeck(userDeckPlayerOne, additionalDeckFour);
+        System.out.println("Player 2:");
+        userDeckPlayerTwo = prepareUserDeck(userDeckPlayerTwo, additionalDeckOne);
+        System.out.println("Player 3:");
+        userDeckPlayerThree = prepareUserDeck(userDeckPlayerThree, additionalDeckTwo);
+        System.out.println("Player 4:");
+        userDeckPlayerFour = prepareUserDeck(userDeckPlayerFour, additionalDeckThree);
+
     }
 
     public static ArrayList<Card> createDeck() {
         deck = new ArrayList<>();
         String[] suit = {"clubs", "diamonds", "hearts", "spades"};
         String[] rank = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
-        for (int i = 0; i < suit.length; i++) {
-            for (int j = 0; j < rank.length; j++) {
-                deck.add(new Card(suit[i], rank[j]));
+        for (String s : suit) {
+            for (String r : rank) {
+                deck.add(new Card(s, r));
             }
         }
         return deck;
@@ -54,25 +79,36 @@ public class Hearts {
         return userDeck;
     }
 
-    public static ArrayList<Card> passThreeCardsToNextPlayer(ArrayList<Card> userDeck){
+    public static ArrayList<Card> passThreeCardsToNextPlayer(ArrayList<Card> userDeck) {
         Scanner scan = new Scanner(System.in);
-        int chosenCards= 0;
-        ArrayList<Card>additionalUserDeck = new ArrayList<>();
-        System.out.println("Your cards:" + userDeck);
-        System.out.println("Choose three cards to pass to next player.");
+        int chosenCards = 0;
+        ArrayList<Card> additionalUserDeck = new ArrayList<>();
+        System.out.println(userDeck);
+        System.out.println("\nChoose three cards to pass to next player.");
         do {
-            System.out.println(chosenCards +1 + ":");
+            System.out.println(chosenCards + 1 + ":");
             System.out.print("\tSuit: ");
             String suit = scan.next();
             System.out.print("\tRank: ");
             String rank = scan.next();
-            for(int i = 0; i < userDeck.size(); i++){
-            if(userDeck.get(i).suit.equalsIgnoreCase(suit) && userDeck.get(i).rank.equalsIgnoreCase(rank)){
-                additionalUserDeck.add(userDeck.remove(i));
-                chosenCards++;}
+            for (int i = 0; i < userDeck.size(); i++) {
+                if (userDeck.get(i).suit.equalsIgnoreCase(suit) && userDeck.get(i).rank.equalsIgnoreCase(rank)) {
+                    additionalUserDeck.add(userDeck.remove(i));
+                    chosenCards++;
+                }
             }
-        }while(chosenCards <3);
-        System.out.println(additionalUserDeck);
+        } while (chosenCards < 3);
+        System.out.println("You passed cards: " + additionalUserDeck);
         return additionalUserDeck;
+    }
+
+    public static void deletePassedCards(ArrayList<Card> userDeck, ArrayList<Card> additionalDeck) {
+        userDeck.removeAll(additionalDeck);
+    }
+
+    public static ArrayList<Card> prepareUserDeck(ArrayList<Card> userDeck, ArrayList<Card> additionalDeck) {
+        userDeck.addAll(additionalDeck);
+        System.out.println(userDeck);
+        return userDeck;
     }
 }
