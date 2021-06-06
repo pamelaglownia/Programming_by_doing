9a27fa36c75aad21bb3d40e25280563f1b79827b
Pam <pamela.glownia@gmail.com>
2021-06-06 18:37:55 +0200

    Solution of first problem from Project Euler

---
+++ src/pl/glownia/pamela/Euler001.java
@@ -0,0 +1,13 @@
+package pl.glownia.pamela;
+
+public class Euler001 {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += i;
+            }
+        }
+        System.out.println(result);
+    }
+}
